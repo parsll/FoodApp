92030e8f29913364a8be562bf301caf32d984271
Santosh Ghimire <50452718+sg606618@users.noreply.github.com>
2023-03-14 20:14:09 +0545

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
-import Footer from "./Components/Footer/Footer";
-import NavBar from "./Components/Header/NavBar";
-import Landing from "./Pages/HomePage/Landing";
-import About from './About Us/About';
-import Program from "./OurProgram/Program";
-
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        {/* <Landing /> */}
-        <About/>
-        {/* <Program/> */}
-        <Routes>
-          <Route />
-        </Routes>
-        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
