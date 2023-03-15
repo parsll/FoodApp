4d7851138f7cf8262dadc1728f04b8256a7407e4
Santosh <sg606618@gmail.com>
2023-03-15 20:59:23 +0545

    changes made

--- client/src/Components/Header/SideBar.jsx
+++ client/src/Components/Header/SideBar.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const SideBar = () => {
   const location = useLocation();
-  console.log('====================================');
+  console.log("====================================");
   console.log(location);
-  console.log('====================================');
-  const admin = 'http://localhost:3000/admin';
-  const dashboard = 'http://localhost:3000/admin/dashboard';
-  const review = 'http://localhost:3000/admin/review';
-  const adddetail = 'http://localhost:3000/admin/adddetail';
-  const hoteldetail = 'http://localhost:3000/admin/hoteldetail';
-  const offer = 'http://localhost:3000/admin/offer';
+  console.log("====================================");
+  const admin = "http://localhost:3000/admin";
+  const dashboard = "http://localhost:3000/admin/dashboard";
+  const donations = "http://localhost:3000/admin/donations";
+  const requests = "http://localhost:3000/admin/requests";
   const urlState = window.location.href;
-  console.log('====================================');
+  console.log("====================================");
   console.log(window.location.pathname);
-  console.log('====================================');
+  console.log("====================================");
   return (
     <>
       <div className="fixed h-full w-fit pt-32">
@@ -23,51 +21,31 @@ const SideBar = () => {
           <div className="flex rounded-md bg-[#f5f5f5]">
             <div
               className={`grid h-14 w-14 cursor-pointer place-items-center rounded-r-[5px] border-l-primary text-black hover:bg-white ${
-                urlState === dashboard ? 'border-l-[.5rem] bg-white' : ''
+                urlState === dashboard ? "border-l-[.5rem] bg-white" : ""
               }`}
             >
               <i className="fa-solid fa-user"></i>
             </div>
           </div>
         </Link>
-        <Link to="/admin/hoteldetail">
+
+        <Link to="/admin/requests">
           <div className="flex rounded-md bg-[#f5f5f5]">
             <div
               className={`grid h-14 w-14 cursor-pointer place-items-center rounded-r-[5px] border-l-primary text-black hover:bg-white ${
-                urlState === hoteldetail ? 'border-l-[.5rem] bg-white' : ''
-              }`}
-            >
-              <i className="fa-regular fa-calendar-days"></i>
-            </div>
-          </div>
-        </Link>
-        <Link to="/admin/offer">
-          <div className="flex rounded-md bg-[#f5f5f5]">
-            <div
-              className={`grid h-14 w-14 cursor-pointer place-items-center rounded-r-[5px] border-l-primary text-black hover:bg-white ${
-                urlState === offer ? 'border-l-[.5rem] bg-white' : ''
+                urlState === requests ? "border-l-[.5rem] bg-white" : ""
               }`}
             >
               <i className="fa-solid fa-badge-percent"></i>
             </div>
           </div>
         </Link>
-        <Link to="/admin/review">
+
+        <Link to="/admin/donations">
           <div className="flex rounded-md bg-[#f5f5f5]">
             <div
               className={`grid h-14 w-14 cursor-pointer place-items-center rounded-r-[5px] border-l-primary text-black hover:bg-white ${
-                urlState === review ? 'border-l-[.5rem] bg-white' : ''
-              }`}
-            >
-              <i className="fa-solid fa-thumbs-up"></i>
-            </div>
-          </div>
-        </Link>
-        <Link to="/admin/adddetail">
-          <div className="flex rounded-md bg-[#f5f5f5]">
-            <div
-              className={`grid h-14 w-14 cursor-pointer place-items-center rounded-r-[5px] border-l-primary text-black hover:bg-white ${
-                urlState === adddetail ? 'border-l-[.5rem] bg-white' : ''
+                urlState === donations ? "border-l-[.5rem] bg-white" : ""
               }`}
             >
               <i className="fa-solid fa-layer-plus"></i>
--- client/src/Pages/AdminPage/Admin.jsx
+++ client/src/Pages/AdminPage/Admin.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Outlet, useLocation } from 'react-router';
-import SideBar from '../../Components/Header/SideBar';
-import NavBar from '../UserProfile/NavBar';
+import React, { useState } from "react";
+import { Outlet, useLocation } from "react-router";
+import SideBar from "../../Components/Header/SideBar";
+import NavBar from "../UserProfile/NavBar";
 
 const Admin = (props) => {
   const location = useLocation();
